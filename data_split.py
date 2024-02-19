14608c47a5499d9a9c7f775499278e2a3f299868
Wang Kesheng <80973094+JustMuteAll@users.noreply.github.com>
2024-02-19 16:30:11 +0800

    Update data_split.py

--- data_split.py
+++ data_split.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import numpy as np
 
 def train_val_test_split(n):
@@ -6,3 +8,41 @@ def train_val_test_split(n):
     val = np.sort(np.random.choice(left, size=int(0.5*len(left)), replace=False))
     test = np.setdiff1d(left, val)
     return train, val, test
+
+# A function to split train and test data in a random sequence,including image and caption, and save them
+def split_data(Img_folder,Caption_file,split_ratio):
+    # read all the images and captions
+    Img_list = os.listdir(Img_folder)
+    Caption_list = np.load(Caption_file,allow_pickle=True)
+    print(len(Img_list))
+    print(len(Caption_list))
+    assert len(Img_list) == len(Caption_list)
+    num = len(Img_list)
+
+    # shuffle the index
+    index = np.arange(num)
+    np.random.shuffle(index)
+
+    # split the data 
+    train_index = index[:int(num*split_ratio)]
+    test_index = index[int(num*split_ratio):]
+    train_Img = [Img_list[i] for i in train_index]
+    test_Img = [Img_list[i] for i in test_index]
+    train_Caption = [Caption_list[i] for i in train_index]
+    test_Caption = [Caption_list[i] for i in test_index]
+    
+    # save the image to seperate folders
+    train_folder = r"C:\Users\DELL\Desktop\Predict\train_img"
+    test_folder = r"C:\Users\DELL\Desktop\Predict\test_img"
+    if not os.path.exists(train_folder):
+        os.makedirs(train_folder)
+    if not os.path.exists(test_folder):
+        os.makedirs(test_folder)
+    for img in train_Img:
+        shutil.copy(os.path.join(Img_folder, img), train_folder)
+    for img in test_Img:
+        shutil.copy(os.path.join(Img_folder, img), test_folder)
+    
+    # save the captions in seperate files
+    np.save(r"C:\Users\DELL\Desktop\Predict\train_Caption.npy",train_Caption)
+    np.save(r"C:\Users\DELL\Desktop\Predict\test_Caption.npy",test_Caption)
